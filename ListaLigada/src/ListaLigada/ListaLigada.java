dc5fcca71f7c332cca5eb2a802de2b3dc6cf09fe
echoMarcoAntonio <m.douglas.enterprises@outlook.com.br>
2025-07-03 17:09:50 -0300

    Feature implementar remoção segura de elementos na lista ligada
    Adicionado método remover(String) com tratamento para remoção no início, meio e fim da lista.
    Atualiza corretamente ponteiros e tamanho da estrutura.

--- ListaLigada/src/ListaLigada/ListaLigada.java
+++ ListaLigada/src/ListaLigada/ListaLigada.java
@@ -44,8 +44,35 @@ public class ListaLigada {
         this.tamanho ++;
     }
 
-    public void remover(String novoValor){
-        // TODO: Implementar lógica de remoção de elemento
+    public void remover(String valorProcurado){
+        Elemento anterior = null;
+        Elemento atual = this.primeiro;
+        for (int i = 0; i < this.getTamanho(); i++){
+            // Cenário 4: Remoção do último elemento da lista.
+            if (this.tamanho == 1){
+                this.primeiro= null;
+                this.ultimo = null;
+            }
+            if (atual.getValor().equalsIgnoreCase(valorProcurado)) {
+                // Cenário 1: Elemento a ser removido é a primeira posição da lista.
+                if (atual == primeiro) {
+                    this.primeiro = atual.getProximo();
+                    atual.setProximo(null);
+                    // Cenario 2: Elemento a ser removido é o último da lista.
+                } else if (atual == ultimo) {
+                    this.ultimo = anterior;
+                    anterior.setProximo(null);
+                } else {
+                    // Cenário 3: Elemento a ser removido não está nas extremidades da lista.
+                    anterior.setProximo(atual.getProximo());
+                    atual = null;
+                }
+                tamanho --;
+                break;
+            }
+            anterior = atual;
+            atual = atual.getProximo();
+        }
     }
 
     public Elemento getPosicao(int posicao){
@@ -53,7 +80,7 @@ public class ListaLigada {
         for (int i = 0; i < posicao; i++){
             // Verifica se há um próximo elemento para avançar na lista
             if (atual.getProximo() != null){
-                atual = atual.getProximo(); // atual passa a apontar para seu próximo elemento
+                atual = atual.getProximo(); // Atual passa a apontar para seu próximo elemento
             }
         }
         return atual;
